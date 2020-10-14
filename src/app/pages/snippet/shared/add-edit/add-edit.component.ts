4bbafa479ac42a67dd701b2924cb88f37e3cd122
Roman Oliynyk <oliynykroma@gmail.com>
2020-10-14 18:41:48 +0300

    moved add form to shared

--- src/app/pages/snippet/shared/add-edit/add-edit.component.ts
+++ src/app/pages/snippet/shared/add-edit/add-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SnippetsService } from 'src/app/services/snippets.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-add-edit',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-edit.component.scss']
 })
 export class AddEditComponent implements OnInit {
-
-  constructor() { }
+  public addSnippet: FormGroup;
+  example = { body: "" };
+  public snippet: any
+  constructor(private fb: FormBuilder,
+    private snippetService: SnippetsService,
+    private userService: UserService) 
+    { }
 
   ngOnInit(): void {
+    this.formInit();
+  }
+
+  formInit() {
+    this.addSnippet = this.fb.group({
+      title: this.fb.control('', Validators.required),
+      description: this.fb.control('', Validators.required),
+      userId: this.fb.control(this.userService.getUser()),
+      lang: this.fb.control('', Validators.required),
+      body: this.fb.control('', Validators.required),
+    });
+  }
+  onSubmit() {
+    this.snippetService.addSnippet(this.addSnippet.value).subscribe((data) => {
+      console.log(data)
+    })
   }
 
 }
