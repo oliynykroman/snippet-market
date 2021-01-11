14bacbe29f1d5f552f40681a78dac15c59e13a0e
Roman oliynyk <oliynykroma@live.com>
2021-01-11 17:52:48 +0200

    pipe tests

--- src/app/pages/snippet/pages/list/list.component.ts
+++ src/app/pages/snippet/pages/list/list.component.ts
@@ -49,3 +49,26 @@ export class ListComponent implements OnInit {
     });
   }
 }
+
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'ebala' })
+
+export class FilterPipe implements PipeTransform {
+
+  transform(items: any[], searchText?: string): any[] {
+    if (!items) {
+      return [];
+    }
+    if (!searchText) {
+      return items;
+    }
+
+    searchText = searchText.toLocaleLowerCase();
+
+    return items.filter(it => {
+      return it.toLocaleLowerCase().includes(searchText);
+    })
+  }
+
+}
--- src/app/pages/snippet/pages/list/list.module.ts
+++ src/app/pages/snippet/pages/list/list.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ListComponent } from './list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FormsModule } from '@angular/forms';
+import { MainPipeModule } from 'src/app/pipes/main-pipe.module';
 
 @NgModule({
   declarations: [
